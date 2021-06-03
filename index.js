32d4cb83e1cd9aad730404cb303f1e0252d56122
xonix <xonixx@gmail.com>
2021-06-03 22:11:50 +0300

    collect youtube links

--- index.js
+++ index.js
@@ -17,7 +17,21 @@ async function getYoutubeLinksOnPage(url) {
     aa.map((a) => a.getAttribute("href"))
   );
 
-  console.info(hrefs)
+  console.info(hrefs.map(extractYoutubeId));
 
   await browser.close();
+}
+
+function extractYoutubeId(url) {
+  const watch = "watch?v=";
+  const i1 = url.indexOf(watch);
+  if (i1 > 0) {
+    return url.substr(i1 + watch.length, 11);
+  }
+  const youtube = "youtu.be/";
+  const i2 = url.indexOf(youtube);
+  if (i2 > 0) {
+    return url.substr(i2 + youtube.length, 11);
+  }
+  throw "Unable to extract yt ID";
 }
