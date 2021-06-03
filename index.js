32be069ea7f84812d8a796b1642fbcce34537e27
xonix <xonixx@gmail.com>
2021-06-03 21:05:10 +0300

    collect youtube links

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
+
+const HEADLESS = false;
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false });
+  const url = process.env.URL;
+  if (!url) throw "Set URL!";
+  await getYoutubeLinksOnPage(url);
+})();
+
+async function getYoutubeLinksOnPage(url) {
+  const browser = await puppeteer.launch({ headless: HEADLESS });
   const page = await browser.newPage();
-  await page.goto('https://example.com');
-  await page.screenshot({ path: 'example.png' });
+  await page.goto(url);
+
+  const hrefs = await page.$$eval("a[href*=youtu]", (aa) =>
+    aa.map((a) => a.getAttribute("href"))
+  );
+
+  console.info(hrefs)
 
   await browser.close();
-})();
+}
